1e5cf6d4b5a4547c8452f648c3333601c2ff0875
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-18 19:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+      var colorSelect = document.getElementById('colorSelect');
+      var selectedIndex = colorSelect.selectedIndex;
+      
+      if (selectedIndex !== -1) {
+        colorSelect.remove(selectedIndex);
+      }
+}
